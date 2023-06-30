d9cf480b73cf226ed808a118a861180a0ee3bcc3
afinyshka <afinyshka@mail.ru>
2023-06-30 23:37:45 +0400

    add hw 10

---
+++ HW_02/index10/data10.js
@@ -0,0 +1,15 @@
+const dataPublic = `{
+    "post code": "90210",
+    "country": "United States",
+    "country abbreviation": "US",
+    "image": "https://www.thecoldwire.com/wp-content/uploads/2021/10/Bunker-Hill-Circa-August-2018-USPS-Post-Office-Location-768x512.jpeg",
+    "places": [
+        {
+            "place name": "Beverly Hills",
+            "longitude": "-118.4065",
+            "state": "California",
+            "state abbreviation": "CA",
+            "latitude": "34.0901"
+        }
+    ]
+ }`
